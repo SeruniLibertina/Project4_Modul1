7b9c61b8b54ec02631cbc270bbc44262f8ccb39c
SeruniLibertina <serunilibertina@gmail.com>
2026-02-16 23:27:31 +0700

    update modul 2

--- lib/features/logbook/counter_view.dart
+++ lib/features/logbook/counter_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:logbook_app_001/features/onboarding/onboarding_view.dart';
-import 'counter_controller.dart';
+import 'package:logbook_app_001/features/logbook/counter_controller.dart';
 
 class CounterView extends StatefulWidget {
   final String username;
